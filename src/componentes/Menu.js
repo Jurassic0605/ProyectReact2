2a97fd5546f3505835b7e098e492583e0a4a1e9a
Jurassic0605 <hermanosscolpino@gmail.com>
2023-08-30 14:12:27 -0300

    primer commit

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -4,22 +4,33 @@ import './menu.css';
 
 export default class Menu extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
-            <div className="row">
-                <div className="col-12 d-flex align-items-center align-self-center bg-success fixed-top py-1 px-4">
-                    <h2> Página </h2>
-                    <Link to="/" className="mx-4 colorLink text-decoration-none text-primary"> Inicio </Link>
-                    <Link to="/personajesMH" className="mx-4 colorLink text-decoration-none text-primary"> Personajes MH </Link>
-                    <Link to="/personajesPK" className="mx-4 colorLink text-decoration-none text-primary"> Personajes PK </Link>
-                    <Link to="/personajesSC" className="mx-4 colorLink text-decoration-none text-primary"> Personajes SC </Link>                 
-                    <Link to="/contacto" className="mx-4 colorLink text-decoration-none text-primary"> Contacto </Link>   
+            <nav className="navbar navbar-expand-lg shadow fixed-top navbar-light bg-light">
+                    <h3>Página</h3>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <Link to="/" className="nav-link mx-4 colorLink text-decoration-none text-primary"> Inicio </Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link to="/personajesMH" className="mx-4 nav-link colorLink text-decoration-none text-primary"> Personajes MH </Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link to="/personajesPK" className="mx-4 nav-link colorLink text-decoration-none text-primary"> Personajes PK </Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link to="/personajesSC" className="mx-4 nav-link colorLink text-decoration-none text-primary"> Personajes SC </Link> 
+                        </li>
+                        <li class="nav-item active">                
+                            <Link to="/contacto" className="mx-4 nav-link colorLink text-decoration-none text-primary"> Contacto </Link>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+            </nav>  
         );
     }
 }
